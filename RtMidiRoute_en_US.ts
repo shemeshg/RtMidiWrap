031ef2d430117b5a934b7b8be89bd78fc0c6dc35
osx user <gili.shemesh@gmail.com>
2021-10-01 09:36:28 +0300

    commit

---
+++ RtMidiRoute_en_US.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>UiMain</name>
+    <message>
+        <location filename="src/uimain.cpp" line="8"/>
+        <source>v1.2.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/uimain.cpp" line="10"/>
+        <source>Status: Running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/uimain.cpp" line="12"/>
+        <source>Status: Not running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/uimain.cpp" line="15"/>
+        <source>Port number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/uimain.cpp" line="19"/>
+        <source>Save and quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/uimain.cpp" line="38"/>
+        <source>Midi Router Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="src/mainclass.cpp" line="39"/>
+        <source>Port for midi server [default: 12345].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/mainclass.cpp" line="40"/>
+        <source>port</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

96fcb347e4e5836283b3f81d7da336a6721d2a94
osx user <gili.shemesh@gmail.com>
2022-09-27 05:12:47 +0300

    commit

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PROJECT_NAME "RtMidiRoute"
-#define PROJECT_VER  "1.3.1"
+#define PROJECT_VER  "1.4.0"
 #define PROJECT_VER_MAJOR "1"
-#define PROJECT_VER_MINOR "3"
-#define PTOJECT_VER_PATCH "1"
+#define PROJECT_VER_MINOR "4"
+#define PTOJECT_VER_PATCH "0"
 
